44a11d157b49da9059af82c13381d0476903365e
fab nom <fabrice.amegadjen@gmail.com>
2021-11-26 21:44:42 +0100

    Procfile

--- Ortensia.py
+++ Ortensia.py
@@ -568,6 +568,7 @@ import traceback
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 TOKEN = os.environ['TOKEN']
